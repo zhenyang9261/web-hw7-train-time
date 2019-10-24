fb17ea172df286710ba9e9e2e3f94b6cda756718
Zhen Data <zhenyang.9261@gmail.com>
2019-10-23 20:48:13 -0400

    Corrected multiple bugs

--- assets/javascripts/traintime.js
+++ assets/javascripts/traintime.js
@@ -18,20 +18,12 @@ var database = firebase.database();
 // Database reference
 var trainRef = database.ref();
 
-// Unique value for database records
-var index = 0;
-
 // HTML input fields
 var trainNameInput, destinationInput, startInput, frequencyInput;
 
 // Variables to hold HTML field values
 var trainName, destination, start, frequency;
 
-// Form field validation error message
-var errMsg = "";
-
-// Array to keep track of all train info
-var trains = [];
 
 /* --------- Functions ---------------- */
 /* 
@@ -39,6 +31,11 @@ var trains = [];
  * Return value: true - all field inputs valid. false - invalid input detected
  */
 function validateInput() {
+
+    trainNameInput = $("#train-name-input");
+    destinationInput = $("#destination-input");
+    startInput = $("#start-input");
+    frequencyInput = $("#frequency-input");
   
     // Get user input
     trainName = trainNameInput.val().trim();
@@ -48,24 +45,20 @@ function validateInput() {
 
     // Fail validation if there is any empty field
     if (trainName.length === 0 || destination.length === 0 || start.length === 0 || frequency.length === 0) {
-      errMsg = "All fields must be filled."
-      $("#info").text(errMsg);
+      $("#info").text("All fields must be filled.");
       return;
     }
 
     // Fail validation if tart time is not in the format of HH:mm
     var timePattern = new RegExp(/^([01]\d|2[0-3]):([0-5]\d)$/);
     if (!timePattern.test(start)) {
-      console.log(start);
-      errMsg = "Please enter start time in the format of military time HH:mm";
-      $("#info").text(errMsg);
+      $("#info").text("Please enter start time in the format of military time HH:mm");
       return;
     }
 
     // Fail validation if frequncy is not a positive number
     if (!(Number.isInteger(frequency)) || frequency <= 0) {
-      errMsg = "Frequency must be a positive integer";
-      $("#info").text(errMsg);
+      $("#info").text("Frequency must be a positive integer");
       return;
     }
 
@@ -74,37 +67,25 @@ function validateInput() {
     query.once("value", function(snapshot) {
 
       if (snapshot.exists()){
-        const userData = snapshot.val();
-        console.log("exists!", userData);
-        errMsg = "Train name exists already."
-        $("#info").text(errMsg);
+        $("#info").text("Train name exists already.");
         return;
       }
       else {
-        console.log("not exist");
+        // Clean the error message display field
         $("#info").text("");
+
+        // Add the train
         addTrain();
       }
-    
-    //   snapshot.forEach(function(child) {
-    //     console.log(child.key);
-    //   });
 
     });
 
-    // trainRef.orderByChild("trainName").equalTo(trainName).once("value",snapshot => {
-    //   if (snapshot.exists()){
-    //     const userData = snapshot.val();
-    //     console.log("exists!", userData);
-    //   }
-    // });
-
-    // All validations passed
     return;
 }
 
 /* 
  * Function: to upload new record in firebase database
+ * Input param: the train object to add
  */
 function addTrain() {
 
@@ -117,14 +98,14 @@ function addTrain() {
     };
 
     // Upload train data to the database
-    trainRef.child(index).set(newTrain);
+    //trainRef.child(index).set(newTrain);
+    trainRef.push(newTrain);
 
     // Clear all of the text-boxes
     trainNameInput.val("");
     destinationInput.val("");
     startInput.val("");
     frequencyInput.val("");
-
 }
 
 /*
@@ -132,16 +113,12 @@ function addTrain() {
  */
 database.ref().on("child_added", function(snapshot) {
   
-  //console.log(snapshot.val());
-
   // Store everything into a variable.
   var trainName = snapshot.val().trainName;
   var destination = snapshot.val().destination;
   var start = snapshot.val().start;
   var frequency = snapshot.val().frequency;
-
-  // Prettify the employee start
-  //var empStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
+  var key = snapshot.key;
 
   // Calculate minutes away. 
   var totalMin = moment().diff(moment(start, "X"), "minutes");
@@ -151,22 +128,21 @@ database.ref().on("child_added", function(snapshot) {
   var currentTime = moment();
   var nextTime = (currentTime.add(minAway, "minutes")).format("HH:mm");
 
-  // Create the remove button
+  // Create the remove button, set value of the button as the child record key for the purposes of edit/remove.
   var removeBtn = $("<button>");
-  removeBtn.attr("value", index);
+  removeBtn.attr("value", key);
   removeBtn.attr("class", "remove-btn");
   removeBtn.text("remove");
 
-  // Create the new row
-  var newRow = $("<tr id='tr-"+ index + "'>").append(
-    $("<td value="+ index + ">").text(trainName),
-    $("<td value="+ index + ">").text(destination),
-    $("<td value="+ index + ">").text(frequency),
-    $("<td value="+ index + ">").text(nextTime),
-    $("<td value="+ index + ">").text(minAway),
-    $("<td value="+ index + ">").append(removeBtn)
+  // Create the new row. Set id of the row as the child record key for the purposes of edit/remove.
+  var newRow = $("<tr id='tr-"+ key + "'>").append(
+    $("<td id='name-"+ key + "'>").text(trainName),
+    $("<td id='dest-"+ key + "'>").text(destination),
+    $("<td id='freq-"+ key + "'>").text(frequency),
+    $("<td id='nexttime-"+ key + "'>").text(nextTime),
+    $("<td id='minaway-"+ key + "'>").text(minAway),
+    $("<td id='remove-"+ key + "'>").append(removeBtn)
   );
-  index++;
 
   // Append the new row to the table
   $("#train-table > tbody").append(newRow);
@@ -177,8 +153,7 @@ database.ref().on("child_added", function(snapshot) {
  * Input param: the id of the child record
  */
 function removeTrain(id) {
-  console.log(id);
-  
+ 
   trainRef.child(id).remove();
   
 }
@@ -187,20 +162,14 @@ function removeTrain(id) {
  * Function: Firebase callback for removing a record to the database. Remove the table row when this happens
  */
 database.ref().on("child_removed", function(snapshot) {
-    console.log(snapshot.key());
-
-    $("#tr-"+snapshot.key()).remove();
+  
+    $("#tr-"+snapshot.key).remove();
 });
 
 /* ----------- Start here -------------- */
 
 $("document").ready(function() {
   
-    trainNameInput = $("#train-name-input");
-    destinationInput = $("#destination-input");
-    startInput = $("#start-input");
-    frequencyInput = $("#frequency-input");
-
     // Add train button listener
     $("#add-train-btn").on("click", function(event) {
       event.preventDefault();
